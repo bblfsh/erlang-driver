85e454645a166ba21181165f873729ecd5b5c8a1
David Paz <dpaz1993@gmail.com>
2017-04-17 10:30:32 +0200

    Deleted autogenerated output in rebar3 run script

--- native/run.sh
+++ native/run.sh
@@ -3,8 +3,11 @@
 #This script is used only for development purposes
 #Run.sh builds the native part of the driver without anything related with bblfs-sdk
 rebar3 release
-awk '{gsub("-noinput","");print}' _build/default/rel/pre-alpha/bin/pre-alpha > _build/default/rel/pre-alpha/bin/tmp
-cat _build/default/rel/pre-alpha/bin/tmp > _build/default/rel/pre-alpha/bin/pre-alpha
-rm _build/default/rel/pre-alpha/bin/tmp
+
+sed -i 's/-noinput/ /'  _build/default/rel/pre-alpha/bin/pre-alpha
+sed -i '/echo "Exec:/ d' _build/default/rel/pre-alpha/bin/pre-alpha
+sed -i '/echo "Root:/ d' _build/default/rel/pre-alpha/bin/pre-alpha
+sed -i '/echo "$RELEASE_ROOT_DIR"/ d' _build/default/rel/pre-alpha/bin/pre-alpha
+
 
 exec _build/default/rel/pre-alpha/bin/pre-alpha foreground
